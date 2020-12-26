adc7acb2e657ae03a3104ac68431689c896af5b9
aleksicstanisha <76488456+aleksicstanisha@users.noreply.github.com>
2020-12-26 15:01:24 +0100

    Add files via upload

---
+++ domasna js/domasna.js
@@ -0,0 +1,68 @@
+// 1.
+carName = 'volvo';
+console.log(carName);
+
+//2.
+x = 75;
+console.log(x);
+
+// 3.
+
+x = 5;
+y = 23;
+sum = x + y;
+console.log(sum);
+document.write(x + y);
+
+// 4.
+
+x = 43;
+y = 72;
+z = x + y;
+alert(z);
+console.log(z);
+
+// 5.
+
+numA = 15;
+numB = 9;
+remainder = numA % numB;
+alert(remainder);
+console.log(remainder);
+
+// 6. 
+
+x = 10;
+y = 5;
+operator = x += y;
+console.log(operator);
+
+// 7.
+
+x = 10;
+y = 5;
+operator2 = x *= y;
+console.log(operator2);
+
+// 8.
+
+let godini = prompt('your age');
+
+if (godini >= 18) {
+    alert('Polnoleten si');
+} else alert('Ne si polnoleten');
+
+// 9.
+
+ x = prompt('random broj');
+
+if ( x % 2 == 0) {
+	alert('Paren broj');
+}else{
+	alert('Neparen broj');
+}
+
+// 10.
+
+//nemozev da se snajdam :(
+
